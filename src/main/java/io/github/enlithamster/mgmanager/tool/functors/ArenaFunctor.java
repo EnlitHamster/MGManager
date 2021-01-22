11ca7fa368cfd4d4986eaa2d2aea022033c0cbd7
Sandro Massa <sandro2398@hotmail.it>
2021-01-22 23:46:15 +0100

    Added arena functor

---
+++ src/main/java/io/github/enlithamster/mgmanager/tool/functors/ArenaFunctor.java
@@ -0,0 +1,9 @@
+package io.github.enlithamster.mgmanager.tool.functors;
+
+import io.github.enlithamster.mgmanager.arena.MGMArena;
+
+public interface ArenaFunctor {
+
+    public void apply(String name, MGMArena arena);
+
+}
